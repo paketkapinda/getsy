f64278b343f1a4575862878d2c424a417bba250f
paketkapinda <genovabuild@gmail.com>
2025-12-14 21:10:08 +0300

    Update index.ts

--- supabase/functions/pod-webhook/index.ts
+++ supabase/functions/pod-webhook/index.ts
@@ -16,7 +16,7 @@ serve(async (req) => {
 
   const supabase = createClient(
     Deno.env.get('SUPABASE_URL')!,
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+    Deno.env.get('SERVICE_ROLE_KEY')!
   )
 
   const { data: order } = await supabase
@@ -49,3 +49,4 @@ serve(async (req) => {
 
   return new Response(JSON.stringify({ success: true }))
 })
+
